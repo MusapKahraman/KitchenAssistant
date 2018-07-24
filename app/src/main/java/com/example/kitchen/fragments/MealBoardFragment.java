808afb6366e847d2e47e34982798419dfe75097d
musap86 <musap86@gmail.com>
2018-07-24 20:14:21 +0300

    feat: Add expandable lists in meal board

--- app/src/main/java/com/example/kitchen/fragments/MealBoardFragment.java
+++ app/src/main/java/com/example/kitchen/fragments/MealBoardFragment.java
@@ -5,8 +5,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,25 +13,26 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.HorizontalScrollView;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 import com.example.kitchen.R;
 import com.example.kitchen.activities.MainActivity;
-import com.example.kitchen.adapters.OnRecipeClickListener;
-import com.example.kitchen.adapters.RecipesAdapter;
-import com.example.kitchen.data.Recipe;
+import com.example.kitchen.adapters.ExpandableListAdapter;
 import com.example.kitchen.utility.KeyUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MealBoardFragment extends Fragment {
 
     private static final String TAG = MealBoardFragment.class.getSimpleName();
-    private static final String RV_STATE = "rv-state";
     private static final String TAB_STATE = "tab-state";
-    private LinearLayoutManager mLayoutManager;
-    private OnRecipeClickListener mClickListener;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
     private TabHost mTabHost;
     private HorizontalScrollView mHorizontalScrollView;
 
@@ -41,15 +40,17 @@ public class MealBoardFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @SuppressWarnings("EmptyMethod")
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        /*
         if (context instanceof OnRecipeClickListener) {
-            mClickListener = (OnRecipeClickListener) context;
         } else {
             throw new ClassCastException(context.toString()
                     + "must implement OnRecipeClickListener");
         }
+        */
     }
 
     @Override
@@ -71,31 +72,31 @@ public class MealBoardFragment extends Fragment {
         mTabHost.setup();
         mTabHost.addTab(mTabHost.newTabSpec("mon")
                 .setIndicator(getString(R.string.monday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("tue")
                 .setIndicator(getString(R.string.tuesday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("wed")
                 .setIndicator(getString(R.string.wednesday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("thu")
                 .setIndicator(getString(R.string.thursday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("fri")
                 .setIndicator(getString(R.string.friday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("sat")
                 .setIndicator(getString(R.string.saturday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.addTab(mTabHost.newTabSpec("sun")
                 .setIndicator(getString(R.string.sunday))
-                .setContent(R.id.rv_tab_content));
+                .setContent(R.id.elv_tab_content));
 
         mTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
             @Override
@@ -117,26 +118,104 @@ public class MealBoardFragment extends Fragment {
         mTabHost.setCurrentTab(1);
         mTabHost.setCurrentTab(0);
 
-        // Dummy view to fill tabs.
-        RecyclerView recyclerView = rootView.findViewById(R.id.rv_tab_content);
-        mLayoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setHasFixedSize(true);
-        RecipesAdapter adapter = new RecipesAdapter(mClickListener);
-        recyclerView.setAdapter(adapter);
+        // get the list view
+        ExpandableListView expListView = rootView.findViewById(R.id.elv_tab_content);
+        // prepare list data
+        prepareListData();
+        // set list adapter
+        ExpandableListAdapter listAdapter = new ExpandableListAdapter(getContext(), listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         Bundle arguments = getArguments();
         if (arguments != null) {
-            List<Recipe> recipes = arguments.getParcelableArrayList(KeyUtils.KEY_RECIPES);
-            adapter.setRecipes(recipes);
             savedInstanceState = arguments.getBundle(KeyUtils.KEY_SAVED_STATE);
             if (savedInstanceState != null) {
                 int tabIndex = savedInstanceState.getInt(TAB_STATE);
                 mTabHost.setCurrentTab(tabIndex);
                 mHorizontalScrollView.setScrollX(getScrollX(tabIndex));
-                mLayoutManager.scrollToPosition(savedInstanceState.getInt(RV_STATE));
             }
         }
         return rootView;
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<>();
+        listDataChild = new HashMap<>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 
     /**
@@ -167,12 +246,10 @@ public class MealBoardFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        mClickListener = null;
         sendToActivity(new Bundle());
     }
 
     private void sendToActivity(Bundle outState) {
-        outState.putInt(RV_STATE, mLayoutManager.findFirstVisibleItemPosition());
         outState.putInt(TAB_STATE, mTabHost.getCurrentTab());
 
         MainActivity activity = null;
@@ -196,6 +273,7 @@ public class MealBoardFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
     }
 
+    @SuppressWarnings("EmptyMethod")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
