1a384eef1c70c5134cc12ae2848d3bf6a3111304
musap86 <musap86@gmail.com>
2018-08-03 16:18:48 +0300

    feat: Load recipe image in collapsing toolbar

--- app/src/main/java/com/example/kitchen/activities/RecipeDetailActivity.java
+++ app/src/main/java/com/example/kitchen/activities/RecipeDetailActivity.java
@@ -7,7 +7,10 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.kitchen.R;
 import com.example.kitchen.data.local.entities.Recipe;
 import com.example.kitchen.utility.AppConstants;
@@ -20,6 +23,7 @@ public class RecipeDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_detail);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,11 +34,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
             isEditable = getIntent().getBooleanExtra(AppConstants.EXTRA_EDITABLE, false);
         }
 
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
             // Change ActionBar title as the name of the recipe.
             actionBar.setTitle(recipe.title);
+        }
+        ImageView recipeImageView = findViewById(R.id.iv_recipe_image);
+        String url = recipe.photoUrl;
+        if (url != null && url.length() != 0) {
+            RequestOptions options = new RequestOptions();
+            Glide.with(this).load(url).apply(options.centerCrop()).into(recipeImageView);
         }
 
         FloatingActionButton fab = findViewById(R.id.fab);
