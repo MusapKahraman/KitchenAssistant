12244973d2b596fa422647d06cbef75a0b87de72
musap86 <musap86@gmail.com>
2018-08-10 21:08:54 +0300

    feat: Handle no user photo

--- app/src/main/java/com/example/kitchen/activities/MainActivity.java
+++ app/src/main/java/com/example/kitchen/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -113,7 +114,12 @@ public class MainActivity extends AppCompatActivity
         TextView userEmail = navHeader.findViewById(R.id.tv_nav_header_subtitle);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-            Glide.with(this).load(user.getPhotoUrl()).into(userImage);
+            Uri uri = user.getPhotoUrl();
+            if (uri != null) {
+                Glide.with(this).load(user.getPhotoUrl()).into(userImage);
+            } else {
+                userImage.setVisibility(View.GONE);
+            }
             userName.setText(user.getDisplayName());
             userEmail.setText(user.getEmail());
         }
